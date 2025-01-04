9bf7d1b4293c5e1f1404ec9151db3c626762a3a2
Raghuraj2004 <raghurajpratapsingh374@gmail.com>
2025-01-04 23:13:37 +0530

    Update UserTest.java

--- src/test/java/com/airoplane/model/UserTest.java
+++ src/test/java/com/airoplane/model/UserTest.java
@@ -1,1 +1,17 @@
+package com.airline.model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @Test
+    void testUserEntity() {
+        User user = new User(1, "john_doe", "password123");
+
+        assertEquals(1, user.getUserId());
+        assertEquals("john_doe", user.getUsername());
+        assertEquals("password123", user.getPassword());
+    }
+}
 
