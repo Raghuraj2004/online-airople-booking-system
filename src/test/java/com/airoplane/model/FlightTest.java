8b8797e7c3155f5373737708f8b9d7cbfe5f12e1
Raghuraj2004 <raghurajpratapsingh374@gmail.com>
2025-01-04 23:13:12 +0530

    Update FlightTest.java

--- src/test/java/com/airoplane/model/FlightTest.java
+++ src/test/java/com/airoplane/model/FlightTest.java
@@ -1,1 +1,18 @@
+package com.airline.model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class FlightTest {
+
+    @Test
+    void testFlightEntity() {
+        Flight flight = new Flight(1, "Flight 101", "New York", "London");
+
+        assertEquals(1, flight.getFlightId());
+        assertEquals("Flight 101", flight.getFlightName());
+        assertEquals("New York", flight.getDepartureCity());
+        assertEquals("London", flight.getArrivalCity());
+    }
+}
 
