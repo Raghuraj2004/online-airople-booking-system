e9deffb4b19c10a646467596de59b75dbab73283
Raghuraj2004 <raghurajpratapsingh374@gmail.com>
2025-01-04 22:08:14 +0530

    Update BookingService.java

--- src/main/java/com/BookingService.java
+++ src/main/java/com/BookingService.java
@@ -1,1 +1,23 @@
+package com.airplane.service;
 
+import com.airplane.dao.BookingDAO;
+import com.airplane.model.Booking;
+
+import java.util.List;
+
+public class BookingService {
+
+    private BookingDAO bookingDAO;
+
+    public BookingService() {
+        this.bookingDAO = new BookingDAO();
+    }
+
+    public void addBooking(Booking booking) {
+        bookingDAO.addBooking(booking);
+    }
+
+    public List<Booking> getAllBookings() {
+        return bookingDAO.getAllBookings();
+    }
+}
