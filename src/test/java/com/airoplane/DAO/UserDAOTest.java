f6978637f2eb5bfd51fd7795b58b41b70b62f38e
Raghuraj2004 <raghurajpratapsingh374@gmail.com>
2025-01-04 23:12:41 +0530

    Update UserDAOTest.java

--- src/test/java/com/airoplane/DAO/UserDAOTest.java
+++ src/test/java/com/airoplane/DAO/UserDAOTest.java
@@ -1,1 +1,33 @@
+package com.airline.dao;
+
+import com.airline.model.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class UserDAOTest {
+
+    @Autowired
+    private UserDAO userDAO;
+
+    @Test
+    void testSaveUser() {
+        User user = new User(1, "john_doe", "password123");
+        userDAO.save(user);
+
+        assertNotNull(userDAO.findById(1).orElse(null));
+    }
+
+    @Test
+    void testFindUserByUsername() {
+        User user = new User(1, "john_doe", "password123");
+        userDAO.save(user);
+
+        User foundUser = userDAO.findByUsername("john_doe").orElse(null);
+        assertNotNull(foundUser);
+        assertEquals("john_doe", foundUser.getUsername());
+    }
+}
 
